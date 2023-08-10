0bce64c5a803707670e0e776cf8c085cbf3f42cb
Diogenes Oliveira <diogeneswilgner.so@gmail.com>
2023-08-10 01:32:15 -0300

    Update tuplas.py

--- tuplas.py
+++ tuplas.py
@@ -11,3 +11,16 @@ frutas = ("laranja", "pera", "uva",)
 letras = tuple("python")
 numeros = tuple([1, 2, 3, 4])
 pais = ("Brasil")
+
+#Exemplo
+frutas = ("maçã", "laranja", "uva", "pera")
+frutas [0] # maçã
+frutas [2] # uva
+
+"""
+Tuplas aninhadas
+Tuplas podem armazenar todos os tipos de objetos Python,
+portanto podemos ter tuplas que armazenam outras tuplas.
+Com isso podemos criar estruturas bidimensionais (tabelas), e
+acessar informando os índices de linha e coluna.
+"""
